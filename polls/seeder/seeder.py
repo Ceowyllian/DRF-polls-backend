83030bccc82635fd04ef98ece856910d2713a390
Ceowyllian <downwell@online.ua>
2022-09-13 16:24:08 +0300

    Update seeder.py

--- polls/seeder/seeder.py
+++ polls/seeder/seeder.py
@@ -13,6 +13,7 @@ from faker import Faker
 from faker.providers import date_time
 
 from polls.models import Question, Choice
+from uuid import uuid4
 
 
 def run(seed=randint(0, 99999)):
@@ -20,11 +21,12 @@ def run(seed=randint(0, 99999)):
     fake = Faker()
     fake.add_provider(date_time)
     seeder = Seed.seeder()
-    seeder.add_entity(Question, 15, {
+    seeder.add_entity(Question, 3, {
         'pub_date': fake.date_time_between('-4d', 'now'),
     })
-    seeder.add_entity(Choice, 40, {
-        'votes': lambda x: randint(0, 100),
+    seeder.add_entity(Choice, 10, {
+        'choice_uuid': lambda x: uuid4(),
+        'votes': lambda x: randint(0, 50),
     })
     seeder.execute()
 
