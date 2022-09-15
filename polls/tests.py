9321f77b354a86d8027bffaac6516bccb2478522
Ceowyllian <downwell@online.ua>
2022-09-15 17:23:50 +0300

    First test

--- polls/tests.py
+++ polls/tests.py
@@ -1,8 +1,15 @@
+import warnings
+from django.http import HttpResponse
 from django.test import TestCase
-
 from .seeder import run
+
 
 class TestQuestionModel(TestCase):
 
     def setUp(self):
+        warnings.simplefilter('ignore')
         run()
+
+    def test_questions(self):
+        response: HttpResponse = self.client.get('/polls/questions/')
+        self.assertEqual(response.status_code, 200)
