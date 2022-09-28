69d3b58e253cfa16139d1a276e6b33d51f64081b
Ceowyllian <downwell@online.ua>
2022-09-28 09:48:09 +0300

    Add models to admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Question
+from .models import (
+    Question,
+    Choice,
+    Vote,
+)
 
 # Register your models here.
 admin.site.register(Question)
+admin.site.register(Choice)
+admin.site.register(Vote)
