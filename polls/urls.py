ac5bb73aa3c3526cb8901522d9aad6cd0116b1e9
Ceowyllian <downwell@online.ua>
2022-10-07 22:51:19 +0300

    Delete "questions/<int:pk>/choices/" endpoint

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,6 @@ from rest_framework.routers import DefaultRouter
 
 from .apiviews import (
     QuestionViewSet,
-    ChoiceList,
     CreateVote,
     UserCreate,
     LoginView,
@@ -14,7 +13,6 @@ router = DefaultRouter()
 router.register('question', QuestionViewSet, basename='question')
 
 urlpatterns = [
-    path('questions/<int:pk>/choices/', ChoiceList.as_view(), name='choice-list'),
     path('questions/<int:pk>/choices/<int:choice_pk>/vote/', CreateVote.as_view(), name='vote-create'),
     path('users/', UserCreate.as_view(), name='user-create'),
     path('login/', LoginView.as_view(), name='login')
