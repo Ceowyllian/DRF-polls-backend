ac634e81283167a8929f630de379a5bab5539ad7
Ceowyllian <downwell@online.ua>
2022-10-13 10:54:10 +0300

    custom_auth models package

---
+++ custom_auth/models/user.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(_("email address"), blank=False, unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    class Meta(AbstractUser.Meta):
+        abstract = False
+        swappable = "AUTH_USER_MODEL"
