21fc334eb9784e155aa4038a34d05502c35db3b7
Ceowyllian <downwell@online.ua>
2022-10-13 11:58:31 +0300

    Remove auth urls

--- polls/urls.py
+++ polls/urls.py
@@ -4,8 +4,6 @@ from rest_framework.routers import DefaultRouter
 from .apiviews import (
     QuestionViewSet,
     VoteView,
-    UserCreate,
-    LoginView,
 )
 
 router = DefaultRouter()
@@ -13,8 +11,6 @@ router.register('question', QuestionViewSet, basename='question')
 
 urlpatterns = [
     path('votes/', VoteView.as_view(), name='vote'),
-    path('users/', UserCreate.as_view(), name='user-create'),
-    path('login/', LoginView.as_view(), name='login')
 ]
 
 urlpatterns += router.urls
