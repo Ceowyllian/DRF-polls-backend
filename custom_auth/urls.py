b0f90c8c5425b623ee3c8361554056740eabc4b8
Ceowyllian <downwell@online.ua>
2022-10-23 19:04:29 +0300

    Delete unused urls

--- custom_auth/urls.py
+++ custom_auth/urls.py
@@ -1,9 +1,5 @@
-from .apiviews import LoginView, UserCreate
-
 from django.urls import path
 
 
 urlpatterns = [
-    path('registration/', UserCreate.as_view(), name='registration'),
-    path('login/', LoginView.as_view(), name='login'),
 ]
