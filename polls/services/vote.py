15848d296f792e740b7aacf8f8feb1c78ed0b9f7
Ceowyllian <downwell@online.ua>
2022-10-25 20:25:56 +0300

    Handle vote-twice-for-the-same-choice error

--- polls/services/vote.py
+++ polls/services/vote.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.db import IntegrityError
 from rest_framework.serializers import ValidationError
 
 from polls.models import Vote
@@ -7,10 +8,14 @@ User = get_user_model()
 
 
 def perform_vote(choice, voted_by) -> Vote:
-    vote, created = Vote.objects.get_or_create(
-        choice=choice,
-        question=choice.question,
-        voted_by=voted_by)
+    try:
+        vote, created = Vote.objects.get_or_create(
+            choice=choice,
+            question=choice.question,
+            voted_by=voted_by
+        )
+    except IntegrityError:
+        raise ValidationError("You can't vote twice for the same question!")
 
     # Raise an error if the vote already exists
     if not created:
