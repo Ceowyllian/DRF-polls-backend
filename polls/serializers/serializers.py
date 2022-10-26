2dc961520f1728c48ee11ad246ce7010fb4abad5
Ceowyllian <downwell@online.ua>
2022-10-26 12:17:02 +0300

    Using "create_question" service in serializer's "create()" method.

--- polls/serializers/serializers.py
+++ polls/serializers/serializers.py
@@ -1,25 +1,23 @@
-from django.db import transaction
 from rest_framework.serializers import (
-    ValidationError,
     ModelSerializer,
 )
 
-from polls import services
 from polls.models import (
     Question,
     Choice,
     Vote,
 )
+from polls.services.question import create_question_with_choices
+from polls.services.vote import perform_vote
 
 
 class VoteSerializer(ModelSerializer):
     class Meta:
         model = Vote
-        fields = '__all__'
-        read_only_fields = ['voted_by', 'date_voted', 'question']
+        fields = ['choice']
 
     def create(self, validated_data):
-        vote = services.vote.perform_vote(**validated_data)
+        vote = perform_vote(**validated_data)
         return vote
 
 
@@ -44,21 +42,22 @@ class QuestionSerializer(ModelSerializer):
 class QuestionWithChoicesSerializer(QuestionSerializer):
     choices = ChoiceSerializer(many=True, required=True)
 
+    default_error_messages = {
+        'too few choices': 'At least 2 choices are required.',
+        'identical choices': 'The answers to the question must be different.',
+    }
+
     class Meta(QuestionSerializer.Meta):
         fields = '__all__'
 
     def validate_choices(self, choices_data):
         choices = [choice['choice_text'] for choice in choices_data]
         if len(choices) < 2:
-            raise ValidationError('At least 2 choices are required.')
+            self.fail('too few choices')
         if len(choices) > len(set(choices)):
-            raise ValidationError('The answers to the question must be different.')
+            self.fail('identical choices')
         return choices_data
 
     def create(self, validated_data):
-        choices_data = validated_data.pop('choices', None)
-        with transaction.atomic():
-            question = Question.objects.create(**validated_data)
-            for choice_data in choices_data:
-                Choice.objects.create(question=question, **choice_data)
+        question = create_question_with_choices(**validated_data)
         return question
