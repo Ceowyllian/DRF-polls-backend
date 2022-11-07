c7000cde622811099fc064f1b2babe386da46dac
Ceowyllian <downwell@online.ua>
2022-11-07 15:43:01 +0200

    Input and output serializers

--- polls/serializers/serializers.py
+++ polls/serializers/serializers.py
@@ -1,63 +1,61 @@
-from rest_framework.serializers import (
-    ModelSerializer,
-)
+from rest_framework import serializers
 
 from polls.models import (
     Question,
     Choice,
-    Vote,
+    ChoiceConfig,
 )
-from polls.services.question import create_question_with_choices
-from polls.services.vote import perform_vote
 
 
-class VoteSerializer(ModelSerializer):
-    class Meta:
-        model = Vote
-        fields = ['choice']
+class ReadOnlyModelSerializer(serializers.ModelSerializer):
+
+    def save(self, **kwargs):
+        raise NotImplementedError
 
     def create(self, validated_data):
-        vote = perform_vote(**validated_data)
-        return vote
+        raise NotImplementedError
+
+    def update(self, instance, validated_data):
+        raise NotImplementedError
 
 
-class ChoiceSerializer(ModelSerializer):
-    class Meta:
-        model = Choice
-        fields = ['choice_text']
-
-
-class QuestionSerializer(ModelSerializer):
+class QuestionDetailSerializer(ReadOnlyModelSerializer):
     class Meta:
         model = Question
-        fields = ['question_title', 'created_by', 'pub_date']
-        read_only_fields = ['created_by', 'pub_date']
-
-    def create(self, validated_data):
-        raise NotImplementedError(
-            "You must implement the creation of questions with multiple choices!"
-        )
-
-
-class QuestionWithChoicesSerializer(QuestionSerializer):
-    choices = ChoiceSerializer(many=True, required=True)
-
-    default_error_messages = {
-        'too few choices': 'At least 2 choices are required.',
-        'identical choices': 'The answers to the question must be different.',
-    }
-
-    class Meta(QuestionSerializer.Meta):
         fields = '__all__'
 
-    def validate_choices(self, choices_data):
-        choices = [choice['choice_text'] for choice in choices_data]
-        if len(choices) < 2:
-            self.fail('too few choices')
-        if len(choices) > len(set(choices)):
-            self.fail('identical choices')
-        return choices_data
+    class ChoiceSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Choice
+            fields = ['choice_text']
 
-    def create(self, validated_data):
-        question = create_question_with_choices(**validated_data)
-        return question
+    choices = ChoiceSerializer(many=True, required=True)
+
+
+class QuestionListSerializer(ReadOnlyModelSerializer):
+    class Meta:
+        model = Question
+        exclude = ['question_text']
+
+
+class QuestionCreateSerializer(ReadOnlyModelSerializer):
+    class Meta:
+        model = Question
+        fields = ['question_title', 'question_text', 'choices']
+
+    choices = serializers.ListSerializer(
+        child=serializers.CharField(
+            min_length=ChoiceConfig.TEXT_MIN_LEN,
+            max_length=ChoiceConfig.TEXT_MAX_LEN
+        ),
+    )
+
+
+class QuestionUpdateSerializer(ReadOnlyModelSerializer):
+    class Meta:
+        model = Question
+        fields = ['question_title', 'question_text']
+
+
+class VoteSerializer(serializers.Serializer):
+    choice_pk = serializers.IntegerField(min_value=1, required=True)
