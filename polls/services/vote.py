23b9b4cf46629f07c419b5049f931ec1cc02c4ae
Ceowyllian <downwell@online.ua>
2022-11-08 12:33:07 +0200

    Update `perform_vote` service

--- polls/services/vote.py
+++ polls/services/vote.py
@@ -1,7 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.db import IntegrityError
-# TODO: replace 'rest_framework.exceptions' with 'django.core.exceptions'
-from rest_framework.exceptions import ValidationError
 
 from polls.models import Vote, Choice
 
@@ -11,9 +8,7 @@ User = get_user_model()
 def perform_vote(choice_pk: int, voted_by: User) -> Vote:
     choice = Choice.objects.select_related('question').get(id=choice_pk)
     vote = Vote(choice=choice, question=choice.question, voted_by=voted_by)
-    vote.full_clean(validate_constraints=False, validate_unique=False)
-    try:
-        vote.save()
-    except IntegrityError:
-        raise ValidationError("You can't vote twice for the same question!")
+
+    vote.full_clean()
+    vote.save()
     return vote
