6d90ed50ab9a9902d1af9885b4f8f77d63c78d14
Ceowyllian <downwell@online.ua>
2022-12-06 21:36:16 +0200

    Ctrl+Alt+L reformat code

--- polls/services/question.py
+++ polls/services/question.py
@@ -40,7 +40,7 @@ def retrieve(*, question_pk: int, fetch_choices: bool = False) -> Question:
     queryset = Question.objects.select_related('created_by')
 
     if fetch_choices:
-        choice_queryset = Choice.objects\
+        choice_queryset = Choice.objects \
             .annotate(num_votes=models.Count('vote'))
         queryset = queryset.prefetch_related(
             models.Prefetch('choice_set', queryset=choice_queryset)
