f6db6dc620975727e41f8937328452e2401fe3a0
Ceowyllian <downwell@online.ua>
2022-12-06 21:14:52 +0200

    Read-only model serializers

---
+++ utils/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+
+class ModelSerializer(serializers.ModelSerializer):
+
+    def save(self, **kwargs):
+        raise NotImplementedError
+
+    def create(self, validated_data):
+        raise NotImplementedError
+
+    def update(self, instance, validated_data):
+        raise NotImplementedError
+
+
+class HyperlinkedModelSerializer(serializers.HyperlinkedModelSerializer):
+
+    def save(self, **kwargs):
+        raise NotImplementedError
+
+    def create(self, validated_data):
+        raise NotImplementedError
+
+    def update(self, instance, validated_data):
+        raise NotImplementedError
