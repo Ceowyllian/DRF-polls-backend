9cba5eb4032a3e551ded574c1dcb939fa31b07ec
Ceowyllian <downwell@online.ua>
2022-12-06 21:28:23 +0200

    Add filtering to `question_list` service, add number of votes to each choice in `retrieve` service.

--- polls/services/question.py
+++ polls/services/question.py
@@ -1,24 +1,60 @@
 from typing import List, Dict, Any
 
-from django.core.exceptions import ValidationError, PermissionDenied
-from django.db import transaction
+import django_filters
+from django.contrib.auth import get_user_model
+from django.core.exceptions import PermissionDenied
+from django.db import transaction, models
 
+from common import UserModelType
 from polls.models import (
     Question,
     Choice,
-    QuestionConfig
+    choice_set_validators
 )
 from utils.services import model_update
 
+User: UserModelType = get_user_model()
 
-def retrieve(question_pk: int, prefetch_choices: object = False) -> Question:
+
+class QuestionFilter(django_filters.FilterSet):
+    created_by = django_filters.ModelChoiceFilter(
+        queryset=User.objects.filter(is_active=True),
+        lookup_expr='username__iexact'
+    )
+    title = django_filters.CharFilter(lookup_expr='icontains')
+    text = django_filters.CharFilter(lookup_expr='icontains')
+    date = django_filters.DateFromToRangeFilter(field_name='pub_date')
+
+    class Meta:
+        model = Question
+        fields = ('title', 'text', 'created_by', 'pub_date')
+
+
+def question_list(*, filters: Dict[str, Any] = None) -> models.QuerySet[Question]:
+    filters = filters or {}
+    qs = Question.objects.all()
+    return QuestionFilter(filters, qs).qs
+
+
+def retrieve(*, question_pk: int, fetch_choices: bool = False) -> Question:
     queryset = Question.objects.select_related('created_by')
-    if prefetch_choices:
-        queryset = queryset.prefetch_related('choice_set')
+
+    if fetch_choices:
+        choice_queryset = Choice.objects\
+            .annotate(num_votes=models.Count('vote'))
+        queryset = queryset.prefetch_related(
+            models.Prefetch('choice_set', queryset=choice_queryset)
+        )
+
     return queryset.get(id=question_pk)
 
 
-def update(question_pk: int, updated_by: Any, data: Dict[str, Any]) -> Question:
+def update(
+        *,
+        question_pk: int,
+        updated_by: UserModelType,
+        data: Dict[str, Any]
+) -> Question:
     question = retrieve(question_pk=question_pk)
     if updated_by != question.created_by:
         raise PermissionDenied("You can't edit this question.")
@@ -31,14 +67,19 @@ def update(question_pk: int, updated_by: Any, data: Dict[str, Any]) -> Question:
     return question
 
 
-def destroy(question_pk: int, destroyed_by: Any):
+def destroy(*, question_pk: int, destroyed_by: UserModelType):
     question = retrieve(question_pk=question_pk)
     if destroyed_by != question.created_by:
         raise PermissionDenied("You can't delete this question.")
     question.delete()
 
 
-def create_question_instance(title: str, text: str, created_by: Any) -> Question:
+def create_question_instance(
+        *,
+        title: str,
+        text: str,
+        created_by: UserModelType
+) -> Question:
     question = Question(
         title=title,
         text=text,
@@ -48,30 +89,40 @@ def create_question_instance(title: str, text: str, created_by: Any) -> Question
     return question
 
 
-def create_choice_instances(choices: List[str], question: Question) -> List[Choice]:
-    number = len(choices)
-    if number > QuestionConfig.CHOICES_MAX_NUMBER:
-        raise ValidationError(
-            'Too many choices, maximum %s allowed.'
-            % QuestionConfig.CHOICES_MAX_NUMBER
-        )
-    if number < QuestionConfig.CHOICES_MIN_NUMBER:
-        raise ValidationError(
-            'Too few choices, minimum %s required.'
-            % QuestionConfig.CHOICES_MIN_NUMBER
-        )
-    if number > len(set(choices)):
-        raise ValidationError('The choices must be different.')
+def create_choice_instances(
+        *,
+        choices: List[str],
+        question: Question
+) -> List[Choice]:
+    for validator in choice_set_validators:
+        validator.__call__(choices)
+
     instances = [Choice(text=text, question=question) for text in choices]
     for instance in instances:
-        instance.full_clean(validate_unique=False, validate_constraints=False)
+        instance.full_clean(
+            validate_unique=False, validate_constraints=False
+        )
     return instances
 
 
-def create(title: str, text: str, created_by: Any, choices: List[str]) -> Question:
-    question = create_question_instance(title, text, created_by)
+def create(
+        *,
+        title: str,
+        text: str,
+        created_by: UserModelType,
+        choices: List[str]
+) -> Question:
+    question = create_question_instance(
+        title=title,
+        text=text,
+        created_by=created_by
+    )
     with transaction.atomic():
         question.save()
-        choice_instances = create_choice_instances(choices=choices, question=question)
+        choice_instances = create_choice_instances(
+            choices=choices, question=question
+        )
         Choice.objects.bulk_create(objs=choice_instances)
-    return retrieve(question_pk=question.pk, prefetch_choices=True)
+    return retrieve(
+        question_pk=question.pk, fetch_choices=True
+    )
