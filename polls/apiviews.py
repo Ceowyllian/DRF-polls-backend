cbb7abc872789d1b96ca682d5dd8306dc7264ea1
Ceowyllian <downwell@online.ua>
2022-12-06 21:29:42 +0200

    Add pagination and handling of query string parameters.

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -6,9 +6,12 @@ from rest_framework.response import Response
 from utils import views
 from . import serializers, services
 from .models import Question
+from .pagination import LimitOffsetPagination, get_paginated_response
 
 
 class QuestionListCreateAPI(views.APIView):
+    class Pagination(LimitOffsetPagination):
+        default_limit = 10
 
     def get_permissions(self):
         if self.request.method == 'GET':
@@ -17,15 +20,29 @@ class QuestionListCreateAPI(views.APIView):
             return [permissions.IsAuthenticated()]
 
     def get(self, request, *args, **kwargs):
-        queryset = Question.objects.all()
-        serializer = serializers.QuestionListSerializer(instance=queryset, many=True)
-        return Response(data=serializer.data)
+        filters_serializer = serializers.QuestionFilterSerializer(
+            data=request.query_params,
+            partial=True,
+        )
+        filters_serializer.is_valid(raise_exception=True)
+        questions = services.question.question_list(
+            filters=filters_serializer.validated_data
+        )
+        return get_paginated_response(
+            pagination_class=self.Pagination,
+            serializer_class=serializers.QuestionListSerializer,
+            queryset=questions,
+            request=request,
+            view=self
+        )
 
     def post(self, request, *args, **kwargs):
         input = serializers.QuestionCreateSerializer(data=request.data)
         input.is_valid(raise_exception=True)
         question = services.question.create(created_by=request.user, **input.validated_data)
-        output = serializers.QuestionDetailSerializer(instance=question)
+        output = serializers.QuestionDetailSerializer(
+            instance=question, context={'request': request}
+        )
         return Response(data=output.data, status=status.HTTP_201_CREATED)
 
 
@@ -38,10 +55,11 @@ class QuestionRetrieveUpdateDeleteAPI(views.APIView):
 
     def get(self, request, *args, **kwargs):
         question = services.question.retrieve(
-            question_pk=kwargs['pk'],
-            prefetch_choices=True,
+            question_pk=kwargs['pk'], fetch_choices=True
         )
-        output = serializers.QuestionDetailSerializer(instance=question)
+        output = serializers.QuestionDetailSerializer(
+            instance=question, context={'request': request}
+        )
         return Response(data=output.data)
 
     def delete(self, request, *args, **kwargs):
@@ -62,7 +80,9 @@ class QuestionRetrieveUpdateDeleteAPI(views.APIView):
             updated_by=self.request.user,
             data=input.validated_data
         )
-        output = serializers.QuestionDetailSerializer(instance=question)
+        output = serializers.QuestionDetailSerializer(
+            instance=question, context={'request': self.request})
+
         return Response(data=output.data)
 
     def patch(self, request, *args, **kwargs):
