eaddf1d468a7cf80e50f6191ed6708dcaca567da
Ceowyllian <downwell@online.ua>
2022-12-07 17:00:18 +0200

    Add HTTP 'delete' method handler to VoteCreateDeleteAPI

--- polls/urls.py
+++ polls/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from .apiviews import (
     QuestionListCreateAPI,
     QuestionRetrieveUpdateDeleteAPI,
-    VoteCreateAPI,
+    VoteCreateDeleteAPI,
 )
 
 urlpatterns = [
     path('questions/', QuestionListCreateAPI.as_view()),
     path('questions/<int:pk>/', QuestionRetrieveUpdateDeleteAPI.as_view(), name='question-detail'),
-    path('votes/', VoteCreateAPI.as_view(), name='vote'),
+    path('votes/<int:pk>/', VoteCreateDeleteAPI.as_view(), name='vote'),
 ]
