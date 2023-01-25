4419714581a630a7bc1592a7c8648ed1dfbbc1fe
Ceowyllian <downwell@online.ua>
2023-01-25 17:16:40 +0200

    Fix (corsheaders.E013) Origin '*' in CORS_ALLOWED_ORIGINS is missing scheme or netloc

--- config/django/base.py
+++ config/django/base.py
@@ -170,4 +170,4 @@ DJOSER = {
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = default_headers + ("cache-control", "cookies")
-CORS_ALLOWED_ORIGINS = ["*"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:8000"]
