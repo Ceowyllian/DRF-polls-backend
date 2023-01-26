d7864ce23f6a8027e5707fd74c464b617419b55c
Ceowyllian <downwell@online.ua>
2023-01-26 15:56:07 +0200

    Rename config class

--- django_polls/users/apps.py
+++ django_polls/users/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CustomAuthConfig(AppConfig):
+class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "django_polls.users"
