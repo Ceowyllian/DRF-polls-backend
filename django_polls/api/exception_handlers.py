6e77a09e5abb1d365c9c73b731bac979fdbfbaa1
Ceowyllian <downwell@online.ua>
2023-01-26 15:53:43 +0200

    Add custom exception handling to API

---
+++ django_polls/api/exception_handlers.py
@@ -0,0 +1,37 @@
+from django.core.exceptions import PermissionDenied
+from django.core.exceptions import ValidationError as DjangoValidationError
+from django.http import Http404
+from rest_framework import exceptions
+from rest_framework.serializers import as_serializer_error
+from rest_framework.views import exception_handler as _exception_handler
+
+
+def exception_handler(exc, ctx):
+    if isinstance(exc, DjangoValidationError):
+        exc = exceptions.ValidationError(as_serializer_error(exc))
+
+    if isinstance(exc, Http404):
+        exc = exceptions.NotFound()
+
+    if isinstance(exc, PermissionDenied):
+        exc = exceptions.PermissionDenied()
+
+    response = _exception_handler(exc, ctx)
+
+    # If unexpected error occurs (server error, etc.)
+    if response is None:
+        return response
+
+    if isinstance(exc.detail, (list, dict)):
+        response.data = {"detail": response.data}
+
+    if isinstance(exc, exceptions.ValidationError):
+        response.data["message"] = "Validation error"
+        response.data["extra"] = {"fields": response.data["detail"]}
+    else:
+        response.data["message"] = response.data["detail"]
+        response.data["extra"] = {}
+
+    del response.data["detail"]
+
+    return response
